b4a3e1ba8c103efaa85daba9802779e698c56d2f
David Björklund <david.bjorklund@gmail.com>
2013-08-18 22:43:33 +0200

    Add failing test for literals over 60 chars

--- test/uncompress-test.js
+++ test/uncompress-test.js
@@ -5,9 +5,10 @@ var fs = require('fs')
 
   , uncompress = require('../lib/uncompress')
 
-  , small   =  fs.readFileSync(__dirname + '/fixtures/small-compressed.bin')
+  , small   = fs.readFileSync(__dirname + '/fixtures/small-compressed.bin')
+  , small2  = fs.readFileSync(__dirname + '/fixtures/small2-compressed.bin')
   , medium  = fs.readFileSync(__dirname + '/fixtures/medium-compressed.bin')
-  , large   =  fs.readFileSync(__dirname + '/fixtures/large-compressed.bin')
+  , large   = fs.readFileSync(__dirname + '/fixtures/large-compressed.bin')
 
 test('uncompress small', function(t) {
   var m = uncompress(small)
@@ -15,6 +16,15 @@ test('uncompress small', function(t) {
 
   t.equal(m.length, data.length, 'should have correct length')
   t.equal(m.toString(), 'foo', 'should have correct data')
+  t.end()
+})
+
+test('uncompress small2', function(t) {
+  var m = uncompress(small2)
+  var data = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@3$%^&*()_'
+
+  t.equal(m.length, data.length, 'should have correct length')
+  t.equal(m.toString(), data, 'should have correct data')
   t.end()
 })
 
