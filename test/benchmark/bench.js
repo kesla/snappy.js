e242d468b1853fdb3c8c5e45a598dc7c03c3ecd3
David Björklund <david.bjorklund@gmail.com>
2013-08-24 16:45:46 +0200

    Benchmark is isValidCompressed also

--- test/benchmark/bench.js
+++ test/benchmark/bench.js
@@ -7,13 +7,24 @@ var fs = require('fs')
 
   , input = fs.readFileSync(path.resolve(__dirname, '../fixtures/urls.10K-compressed.bin'))
 
+console.log('uncompress')
 console.time('snappy')
-for(var i = 0; i < 10; ++i)
+for(var i = 0; i < 100; ++i)
   snappy.uncompressSync(input)
 console.timeEnd('snappy')
 
 console.time('snappy.js')
-for(var i = 0; i < 10; ++i)
+for(var i = 0; i < 100; ++i)
   snappyjs.uncompress(input)
+console.timeEnd('snappy.js')
 
+console.log('isValidCompressed')
+console.time('snappy')
+for(var i = 0; i < 100; ++i)
+  snappy.isValidCompressedSync(input)
+console.timeEnd('snappy')
+
+console.time('snappy.js')
+for(var i = 0; i < 100; ++i)
+  snappyjs.isValidCompressed(input)
 console.timeEnd('snappy.js')
