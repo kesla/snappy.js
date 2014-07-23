164a6be38d8ca7abd196057220daf0ef6f159eb4
David Björklund <david.bjorklund@gmail.com>
2014-07-23 23:38:38 +0200

    add failing test
    Conflicts:
    	package.json

--- test/uncompress-test.js
+++ test/uncompress-test.js
@@ -1,10 +1,13 @@
 
 var fs = require('fs')
 
+  , snappy = require('snappy')
   , test = require('tape')
 
   , uncompress = require('../lib/uncompress')
 
+  , alice   = fs.readFileSync(__dirname + '/fixtures/alice29.txt')
+  , mr      = fs.readFileSync(__dirname + '/fixtures/mapreduce-osdi-1.pdf')
   , small   = fs.readFileSync(__dirname + '/fixtures/small-compressed.bin')
   , small2  = fs.readFileSync(__dirname + '/fixtures/small2-compressed.bin')
   , random  = fs.readFileSync(__dirname + '/fixtures/random-compressed.bin')
@@ -14,6 +17,38 @@ var fs = require('fs')
 
   , randomRaw = fs.readFileSync(__dirname + '/fixtures/random-raw.bin')
   , urlsRaw   = fs.readFileSync(__dirname + '/fixtures/urls.10K-raw.txt')
+
+test('uncompress alice', function(t) {
+  snappy.compress(alice, function(err, compressed) {
+    snappy.uncompress(compressed, function(err, m) {
+      // var m = uncompress(compressed);
+
+      t.equal(m.length, alice.length, 'should have correct length')
+      t.equal(m.toString(), alice.toString(), 'should have correct data')
+      t.end()
+
+    });
+
+  })
+})
+
+test('uncompress mr', function(t) {
+  snappy.compress(mr, function(err, compressed) {
+
+    snappy.uncompress(compressed, function(err, m) {
+
+      t.equal(m.length, mr.length, 'should have correct length')
+      t.equal(m.toString(), mr.toString(), 'should have correct data')
+
+      var m = uncompress(compressed);
+
+      console.log('m', m)
+
+
+      t.end()
+    })
+  })
+})
 
 test('uncompress small', function(t) {
   var m = uncompress(small)
